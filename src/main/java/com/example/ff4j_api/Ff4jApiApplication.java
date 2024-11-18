5cce5aef72f4dcef0a3cb7bfbb52ad81a74846c6
ItsJotas <costajhonata8@gmail.com>
2024-11-18 16:24:13 -0300

    #01[feature] - Configurações básicas

---
+++ src/main/java/com/example/ff4j_api/Ff4jApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ff4j_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff4jApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ff4jApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ff4j_api/Ff4jApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ff4j_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff4jApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
